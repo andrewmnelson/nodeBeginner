5c7eff581e3148355e5f1579b80f2c5821a80294
Andrew Nelson <andrewnelson@earthlink.net>
2015-09-06 14:15:41 -0700

    Responding with non-blocking ops

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,17 +1,38 @@
 'use strict';
 
+var exec = require('child_process').exec;
+
 function logReq(name) {
   console.log("Request handler '" + name + "' was called.")
 };
 
-function start() {
-  logReq("start");
-  return "Hello Fellow.";
+function writeTextContent(resp, content) {
+  resp.writeHead(200, {"Content-Type": "text/plain"});
+  resp.write(content);
+  resp.end();
 };
 
-function upload() {
+// function sleep(milliseconds) {
+//   var startTime = new Date().getTime();
+//   while (new Date().getTime() < startTime + milliseconds) {
+//     ; // just spin uncomfortably here for the moment
+//   }
+// };
+
+function start(response) {
+  logReq("start");
+  //sleep(5000);
+
+  exec("find /",
+    { timeout: 10000, maxBuffer: 20000 * 1024 },
+    function(error, stdout, stderr) {
+      writeTextContent(response, stdout);
+  });
+};
+
+function upload(response) {
   logReq("upload");
-  return "Are you Mellow?";
+  writeTextContent(response, "Chill yer drill, Bill");
 };
 
 exports.start = start;
